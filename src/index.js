c309f520b7beadafc2454fc9c53c91ee96560a38
ibrahim <=>
2021-02-26 17:36:05 +0200

    add fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-todo">
+    <BrowserRouter>
         <App />
     </BrowserRouter>
   </React.StrictMode>,
